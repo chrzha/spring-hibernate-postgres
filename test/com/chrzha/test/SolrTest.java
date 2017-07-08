b5d787aff579ed5bea4762bd9fff37359eedc531
chrzha <chrzha@163.com>
2017-07-08 21:27:29 +0800

    [SHP] refine query parameter
    add rows num to override default value: 10

--- test/com/chrzha/test/SolrTest.java
+++ test/com/chrzha/test/SolrTest.java
@@ -4,11 +4,14 @@ import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.client.solrj.impl.XMLResponseParser;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.params.MapSolrParams;
+import org.apache.solr.common.params.SolrParams;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Chris Zhang on 2017/7/8.
@@ -18,9 +21,15 @@ public class SolrTest {
         String urlString = "http://localhost:8080/solr/core-test";
         SolrClient solr = new HttpSolrClient(urlString);
         try {
-            QueryResponse resp = solr.query(new SolrQuery("username:postman"));
+            SolrQuery query = new SolrQuery();
+            Map<String, String> pmap = new HashMap<String, String>();
+            pmap.put("q", "username:postman");
+            pmap.put("rows", "20");
+            SolrParams params = new MapSolrParams(pmap);
+            query.add(params);
+            QueryResponse resp = solr.query(query);
             SolrDocumentList list = resp.getResults();
-            System.out.println("===========");
+            System.out.println("===========" + list.size());
         } catch (SolrServerException e) {
             e.printStackTrace();
         } catch (IOException e) {
